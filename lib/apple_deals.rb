2fbaa5dc7f9eb5f44090a34408888b7dafe8c939
Brianna Jordan <bryvjordan9@gmail.com>
2019-11-04 02:11:59 +0000

    first commit

---
+++ lib/apple_deals.rb
@@ -0,0 +1,8 @@
+
+require_relative "./apple_deals/version"
+require_relatve './apple_deals/cli'
+
+module AppleDeals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/apple_deals/version.rb
@@ -0,0 +1,3 @@
+module AppleDeals
+  VERSION = "0.1.0"
+end

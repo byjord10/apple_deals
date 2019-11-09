20b367d117b0e0e85088c2968319351e4e9282b5
Brianna Jordan <bryvjordan9@gmail.com>
2019-11-09 20:19:24 +0000

    fixed syntax error after repository merge. Now getting (NameError) git push

--- lib/apple_deals.rb
+++ lib/apple_deals.rb
@@ -2,14 +2,3 @@
 require_relative  "./apple_deals/version"
 require_relative  './deals'
 require_relative  './cli'
-
-
-
-require_relative "./apple_deals/version"
-require_relatve './apple_deals/cli'
-
-module AppleDeals
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
--- lib/cli.rb
+++ lib/cli.rb
@@ -35,27 +35,6 @@ end
     puts "Goodbye! "
     puts "We'll be back with weekly Apple Deals!"
   end 
-  
 end 
 
-apple_deals::CLI.new.call 
-
-class AppleDeals::CLI 
-  
-  def call 
-    puts "Current Apple Brand Deals Available:"
-    available_deals
-  end 
-  
-  def available_deals 
-    puts <<-DOC.gsub/^\s*/,''
-    1. New Apple iPad (10.2-Inch, Wi-Fi, 128GB) - Silver (Latest Model)
-    2. Apple Watch Series 3 (GPS, 38mm) - Space Gray Aluminium Case with Black Sport Band
-    3. Apple AirPods with Charging Case (Latest Model)
-    4. Beats by Dr. Dre - Beats Solo³ Wireless Headphones - Rose Gold
-    5. Beats by Dr. Dre - urBeats³ Earphones with Lightning Connector - Black
-    
-    
-    -DOC
-  end 
 

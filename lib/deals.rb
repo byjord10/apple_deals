aa2a815fd3c246dbfe225fb5b7c9161d983c6e6e
Brianna Jordan <bryvjordan9@gmail.com>
2019-11-13 15:56:37 +0000

    subbed for macbook sales

--- lib/deals.rb
+++ lib/deals.rb
@@ -3,36 +3,36 @@ class AppleDeals::Deals
 
   def self.today
   #  puts <<-DOC
-    #  1. New Apple iPad (10.2-Inch, Wi-Fi, 128GB) - Silver (Latest Model)
-    #  2. Apple Watch Series 3 (GPS, 38mm) - Space Gray Aluminium Case with Black Sport Band
-    #  3. Apple AirPods with Charging Case (Latest Model)
-    #  4. Beats Studio3 Wireless Headphones - Porcelain Rose (Renewed)
+    #  1. New Apple MacBook Air (13-inch, 1.6GHz dual-core Intel Core i5, 8GB RAM, 128GB) - Space Gray
+    #  2. New Apple MacBook Pro (13-inch, 8GB RAM, 256GB Storage) - Space Gray
+    #  3. New Apple MacBook Pro (15-inch, 16GB RAM, 256GB Storage) - Space Gray
+    #  4.Apple Magic Mouse 2 (Wireless, Rechargable) - Space Gray
 
   #  DOC
 
     deal_1 = self.new
-    deal_1.name = "New Apple iPad (10.2-Inch, Wi-Fi, 128GB) - Silver (Latest Model)"
-    deal_1.price = "$399.98"
+    deal_1.name = "New Apple MacBook Air (13-inch, 1.6GHz dual-core Intel Core i5, 8GB RAM, 128GB) - Space Gray"
+    deal_1.price = "$999.00"
     deal_1.availability = true
-    deal_1.url = "https://www.amazon.com/Apple-iPad-10-2-Inch-Wi-Fi-128GB/dp/B07XR5B32Q/ref=sr_1_1?keywords=apple&qid=1572831790&refinements=p_n_condition-type%3A2224371011&s=electronics&sr=1-1 "
+    deal_1.url = "https://www.amazon.com/dp/B07V1PHKPM/ref=fs_a_mb_1 "
 
     deal_2 = self.new
     deal_2.name = "Apple Watch Series 3 (GPS, 38mm) - Space Gray Aluminium Case with Black Sport Band"
-    deal_2.price = "$189"
+    deal_2.price = "	$1,599.00 "
     deal_2.availability = true
-    deal_2.url = "https://www.amazon.com/Apple-Watch-GPS-38mm-Space-Aluminium/dp/B07K39FRSL/ref=sr_1_3?keywords=apple&qid=1573086108&refinements=p_n_condition-type%3A2224371011&s=electronics&sr=1-3 "
+    deal_2.url = "https://www.amazon.com/dp/B07S1YPSGT/ref=fs_a_mb_2"
 
     deal_3 = self.new
-    deal_3.name = "Apple AirPods with Charging Case (Latest Model)"
-    deal_3.price = "$144"
+    deal_3.name = "New Apple MacBook Pro (15-inch, 16GB RAM, 256GB Storage) - Space Gray"
+    deal_3.price = "$2,099.99"
     deal_3.availability = true
-    deal_3.url = "https://www.amazon.com/Apple-AirPods-Charging-Latest-Model/dp/B07PXGQC1Q/ref=sr_1_4?keywords=apple&qid=1573086108&refinements=p_n_condition-type%3A2224371011&s=electronics&sr=1-4"
+    deal_3.url = "https://www.amazon.com/dp/B07RZWHHHP/ref=fs_a_mb_3"
 
     deal_4 = self.new
-    deal_4.name = "Beats Studio3 Wireless Headphones - Porcelain Rose (Renewed)"
-    deal_4.price = "$179.99"
+    deal_4.name = "Apple Magic Mouse 2 (Wireless, Rechargable) - Space Gray"
+    deal_4.price = "$86.00 "
     deal_4.availability = true
-    deal_4.url = "https://www.amazon.com/Beats-Studio3-Wireless-Headphones-Porcelain/dp/B07G2Q6G3R/ref=sr_1_6?keywords=Beats+by+Dr.+Dre+-+Beats+Solo%C2%B3+Wireless+Headphones+-+Rose+Gold&qid=1573086466&s=electronics&sr=1-6"
+    deal_4.url = "https://www.amazon.com/Apple-Magic-Mouse-Wireless-Rechargable/dp/B07BR94PPD?ref_=ast_sto_dp&th=1&psc=1"
 
     [deal_1, deal_2, deal_3, deal_4]
   end
